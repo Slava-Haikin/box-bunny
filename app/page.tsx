e64c6d37a1b5f32401ac53ede55ade8994e05f39
Slava Haikin <vyachowski@gmail.com>
2025-06-03 00:44:48 +0300

    feat: add basic components and main layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 export default function Home() {
   return (
     <Container>
-      <div className="flex w-full max-w-sm flex-col gap-6 items-center">
+      <div className="flex w-full flex-col gap-6 items-center">
         <Tabs defaultValue="grocery-list" className="w-full">
           <TabsList className="w-full">
             <TabsTrigger value="grocery-list">Grocery list</TabsTrigger>
--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 export function Container ({ children, className }: { children: React.ReactNode; className?: string }) {
     return (
-        <div className={cn("p-4 max-w-7xl", className)}>
+        <div className={cn("p-4 max-w-7xl mx-auto", className)}>
             {children}
         </div>
     )

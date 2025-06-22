a798f6399804ed61cb196d63ef8bafd2964d02a0
Slava Haikin <vyachowski@gmail.com>
2025-06-22 22:40:15 +0300

    feat: add correct imports

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,4 @@
-// import { Ingredient } from "@/database/src/entity/Ingredient";
-// import { Recipe } from "@/database/src/entity/Recipe";
-
-import { ingredients, recipes } from "@/db/schema";
+import { ingredientsTable, recipesTable } from "@/db/schema";
 
 enum COOKING_DIFFICULTY {
     easy = 'easy',
@@ -23,40 +20,8 @@ enum RecipeTag {
   VEGAN = 'vegan',
 }
 
-// interface Recipe {
-//     id: string;
-//     title: string;
-//     description: string;
-//     instructions: string[];
-//     meal: MEAL;
-//     difficulty: COOKING_DIFFICULTY;
-//     cookTime: number;
-//     servings: number;
-//     imageUrl: string;
-//     tags: 'gluten free' | 'vegetarian' | 'vegan'[];
-// }
-
-// interface Ingredient {
-//     id: string;
-//     original: string;
-//     name: string;
-//     unit: string | null;
-//     unit_short: string | null;
-//     unit_long: string | null;
-//     possible_units: string | null;
-//     quantity: number;
-//     estimated_cost_value: number | null;
-//     estimated_cost_unit: string | null;
-//     consistency: string | null;
-//     shopping_list_units: string | null;
-//     aisle: string | null;
-//     image_url: string | null;
-//     created_at: string;
-//     updated_at: string;
-// }
-
-type Ingredient =  typeof ingredients.$inferSelect & { quantity?: number };
-type Recipe = typeof recipes.$inferSelect;
+type Ingredient =  typeof ingredientsTable.$inferSelect & { quantity?: number };
+type Recipe = typeof recipesTable.$inferSelect;
 
 interface MealPlan {
     [MEAL.breakfast]: Recipe;

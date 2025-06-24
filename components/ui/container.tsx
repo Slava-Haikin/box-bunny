8baaf5d7ed4ba2a3f1988c238cacd23badbd53e1
Slava Haikin <vyachowski@gmail.com>
2025-06-24 21:10:48 +0300

    style: add new container max width(mobile only)

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -8,7 +8,7 @@ export function Container({
   className?: string;
 }) {
   return (
-    <div className={cn("py-4 px-6 max-w-7xl mx-auto", className)}>
+    <div className={cn("py-4 px-6 max-w-xl mx-auto", className)}>
       {children}
     </div>
   );
